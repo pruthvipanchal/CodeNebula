d3c0cdfcc6fe1be2809ca2a316874b4eb89840b7
Pruthvi Panchal <pruthvipanchal46@gmail.com>
2026-03-29 02:13:59 +0530

    Update copyright year in footer of config.ts

--- site/.vitepress/config.ts
+++ site/.vitepress/config.ts
@@ -69,7 +69,7 @@ export default defineConfig({
 
     footer: {
       message: "Released under the MIT License.",
-      copyright: "Copyright © 2024-present Pruthvi Panchal · CodeNebula",
+      copyright: "Copyright © 2026-present Pruthvi Panchal · CodeNebula",
     },
 
     editLink: {
